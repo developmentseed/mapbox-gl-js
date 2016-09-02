f41be04b74bfacf8af5f16ae404fde1e9063f348
Anand Thakker <vestibule@anandthakker.net>
2016-09-02 12:00:08 -0400

    Update make-fork.sh

--- make-fork.sh
+++ make-fork.sh
@@ -12,7 +12,10 @@ hub merge https://github.com/mapbox/mapbox-gl-js/pull/2982
 echo Bucket enhancements to allow property-only update
 hub merge https://github.com/mapbox/mapbox-gl-js/pull/2812
 
+echo Patch WebWorker to enable custom source type in node usage
+git cherry-pick a69e8730daaac364f652c682c13a653db2f852c4
+
 # re-add fork readme and this script
-git checkout origin/wapo make-fork.sh
-git checkout origin/wapo README.md
+git checkout origin/wapo-2016-09-01 make-fork.sh
+git checkout origin/wapo-2016-09-01 README.md
 git commit -am "Add fork readme and make-fork.sh"
